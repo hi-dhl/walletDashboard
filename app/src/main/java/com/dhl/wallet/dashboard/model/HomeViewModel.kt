548df0bd93da300ce0091dac3196cb115002a0d7
hi-dhl <hihuilong@gmail.com>
2025-07-16 00:28:50 +0800

    初始化项目结构

---
+++ app/src/main/java/com/dhl/wallet/dashboard/model/HomeViewModel.kt
@@ -0,0 +1,13 @@
+package com.dhl.wallet.dashboard.model
+
+import androidx.lifecycle.ViewModel
+
+/**
+ * <pre>
+ *     author: dhl
+ *     date  : 2025/7/15
+ *     desc  :
+ * </pre>
+ */
+class HomeViewModel : ViewModel() {
+}
---
+++ app/src/main/java/com/dhl/wallet/dashboard/ui/HomeActivity.kt
@@ -0,0 +1,20 @@
+package com.dhl.wallet.dashboard.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.dhl.wallet.dashboard.R
+
+/**
+ * <pre>
+ *     author: dhl
+ *     date  : 2025/7/15
+ *     desc  :
+ * </pre>
+ */
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+
+}
